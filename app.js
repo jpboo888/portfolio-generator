c737a5fef64e55089cc1f6529f1048c3ca00db8d
JP MALDONADO <jpmaldonado@jps-imac.lan>
2022-06-30 14:03:09 -0500

    added 2nd js page

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+const fs = require('fs');
+const generatePage = requre('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const [name, github] = profileDataArgs;
 
-  console.log('================');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+
+
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
